cf21b76b20694ada38eb20ca2840656a9fe085fe
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-14 17:54:12 +0500

    Netflix-Clone-React

---
+++ src/Components/Footer.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                <div className="questions">Questions? Contact Us</div>
+                <div className="footer">
+                    <div className="footer-item">
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">MSR</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Muhammad Shahmeer</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Privacy</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Speed Test</a>
+                    </div>
+
+                    <div className="footer-item">
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Help Centre</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Jobs</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">cookie prefrences</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Legal Notices</a>
+                    </div>
+                    <div className="footer-item">
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Account</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Ways to Watch</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Corporate Information</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Only on Netflix</a>
+                    </div>
+
+                    <div className="footer-item">
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Media center</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Term of Use</a>
+                        <a target='blank' href="https://www.linkedin.com/in/muhammadshahmeerrizwan/">Contact Us</a>
+                    </div>
+                    <div className="footer-item">
+                        <button className="footer-btn">
+                            English &nbsp;&nbsp;&nbsp;&nbsp;<i
+                                className="fa-solid fa-caret-down"
+                            ></i>
+                        </button>
+                        <div>Netflix on Pakistan </div>
+                        <div>Made By  </div>
+                        <div>Shahmeer Rizwan </div>
+
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
